4b82f8d7645a3625d0898d1d1155a54ccb6cdf87
nachomac12 <macias.ignacio@hotmail.com>
2019-08-23 14:50:03 -0300

    Update carousel

--- client/src/components/Home/carousel.js
+++ client/src/components/Home/carousel.js
@@ -1,40 +1,47 @@
 import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import { Link } from 'react-router-dom';
-import '../Main/main.css';
+import { Link } from "react-router-dom";
+import "./carousel.css";
+import LeftArrow from "@material-ui/icons/KeyboardArrowLeft";
+import RightArrow from "@material-ui/icons/KeyboardArrowRight";
 
 export default () => (
-  <Carousel 
-    autoPlay={true} 
-    infiniteLoop={true} 
-    showThumbs={false} 
-    showStatus={false} 
-    useKeyboardArrows={true}
-    showIndicators={false}
-    transitionTime={700}
-    interval={10000}
-    stopOnHover={false}
-  >
-    <div>
-      <img style={{opacity: '0.6'}} 
-        src="/images/fotolibu2.jpg" alt="img" />
-        <div className="col-md-5 FotoUno">
+  <div id="carouselHome" className="carousel slide" data-keyboard={true} data-ride="carousel">
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <div className="FotoUno col-md-5">
           <h1 className="TextoHome">Emprende tu camino como profesional</h1>
           <p>Crea una cuenta y de esa manera podrás obtener trabajos, así como también
           gestionarlos y tener un feedback con las personas o empresas que te contraten</p>
           <Link to="/registro_profesional" className="btn btn-info btn-lg btnCarouselUno">Registrarme como profesional</Link>
         </div>
-    </div>
-    <div>
-      <img style={{opacity: '0.6'}} 
-        src="/images/fotolibu3.jpg" alt="img" />
-        <div className="col-md-5 FotoDos">
+        <img className="d-block w-100" src="/images/fotolibu2.jpg" alt="img"/>
+      </div>
+      <div className="carousel-item">
+        <div className="FotoDos col-md-5">
           <h1 className="TextoHome">Contrata profesionales para realizar un trabajo</h1>
           <p>Si tienes la necesidad de contar con algún profesional para resolver tu demanda personal o
             empresarial puedes registrarte como Empleador y de esa manera contratar a los trabajadores aptos
             para la labor que requieres.</p>
           <Link to="/registro_empleador" className="btn btn-info btn-lg btnCarouselDos">Registrarme como empleador</Link>
         </div>
+        <img className="d-block w-100" src="/images/fotolibu3.jpg" alt="img" />
+      </div>
     </div>
-  </Carousel>
+    <a
+      className="carousel-control-prev"
+      href="#carouselHome"
+      role="button"
+      data-slide="prev"
+    >
+      <div className="LeftArrow"><LeftArrow id="LeftArrowIcon"/></div>
+    </a>
+    <a
+      className="carousel-control-next"
+      href="#carouselHome"
+      role="button"
+      data-slide="next"
+    >
+      <div className="RightArrow"><RightArrow id="RightArrowIcon"/></div>
+    </a>
+  </div>
 );
